a8351ce7681affee1e991100a0cfe5d8108d0729
Vikki <yingjialiu1216@gmail.com>
2025-03-12 14:02:37 -0700

    refactor: submit tweet using jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,29 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function () {
+  console.log("Listening tweet form.")
+  // select the form id tweet-form to listen for the submit event
+  const tweetForm = document.querySelector("#tweet-form");
+  tweetForm.addEventListener("submit", async function (event) {
+    event.preventDefault(); // prevent default form submission behavior
+    
+    // serialize the form data
+    const formData = $(this).serialize();
+
+    // send an ajax POST request
+    $.ajax ({
+      url: "/api/tweets",
+      method: "POST",
+      data: formData,
+      success: function (response) {
+        console.log(`Tweet submitted successfully: ${formData}`, response);
+        // clear the form after submission
+        $("#tweet-text").val(""); 
+      },
+      error: function(xhr, status, error) {
+        console.log("Error submitting tweet:", xhr.responseText);
+      }
+    })
+  });
+});
