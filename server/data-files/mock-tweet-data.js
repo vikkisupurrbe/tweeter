f9401584cca321ee5b361dd1511b377fa42fc303
Vikki <yingjialiu1216@gmail.com>
2025-03-12 00:09:28 -0700

    chore: upload mock tweet data

---
+++ server/data-files/mock-tweet-data.js
@@ -0,0 +1,62 @@
+const tweets = [
+  {
+    nickname: "Descartes",
+    username: "@rd",
+    tweetText: "I think, therefore I tweet.",
+    timePosted: "2 hours ago"
+  },
+  {
+    nickname: "Rosa Frilli",
+    username: "@Frilli20",
+    tweetText: "The sunset today reminded me why I love photography.",
+    timePosted: "just now"
+  },
+  {
+    nickname: "Alex Morgan",
+    username: "@alexonthego",
+    tweetText: "All that glitters is not gold, but this new phone sure does shine!",
+    timePosted: "1 day ago"
+  },
+  {
+    nickname: "Jamie Chen",
+    username: "@jc_codes",
+    tweetText: "Debugging: being the detective in a crime scene where you're also the murderer.",
+    timePosted: "5 minutes ago"
+  },
+  {
+    nickname: "SophiaWise",
+    username: "@sophia_w",
+    tweetText: "Books are the quietest and most constant of friends.",
+    timePosted: "3 days ago"
+  },
+  {
+    nickname: "Marcus Bell",
+    username: "@bellringer",
+    tweetText: "Just tried that new café downtown. The coffee? Life-changing.",
+    timePosted: "7 hours ago"
+  },
+  {
+    nickname: "Elena Vega",
+    username: "@ev_stars",
+    tweetText: "Sometimes the universe gives you exactly what you need, not what you want.",
+    timePosted: "2 days ago"
+  },
+  {
+    nickname: "ThoughtfulTom",
+    username: "@tom_thinks",
+    tweetText: "The hardest thing about learning something new is unlearning what you thought you knew.",
+    timePosted: "45 minutes ago"
+  },
+  {
+    nickname: "Priya Mehta",
+    username: "@priyacooks",
+    tweetText: "My grandmother's recipe collection is my most treasured inheritance.",
+    timePosted: "6 hours ago"
+  },
+  {
+    nickname: "Jackson Wright",
+    username: "@jwright_now",
+    tweetText: "The early bird gets the worm, but the second mouse gets the cheese.",
+    timePosted: "4 days ago"
+  }
+];
