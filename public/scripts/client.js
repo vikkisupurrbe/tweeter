8014da58b388f1c0b1356d20ddabc8fc147bfd50
Vikki <yingjialiu1216@gmail.com>
2025-03-12 17:45:09 -0700

    refatctor: load tweets using ajax, fix time passed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -30,37 +30,12 @@ $(document).ready(function () {
       error: function(xhr, status, error) {
         console.log("Error submitting tweet:", xhr.responseText);
       }
-      
+
     })
   });
 
   // dynamically fetching tweets
   // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-  
   const createTweetElement = function(tweet) {
     let $tweet = $(`
       <article class="tweet">
@@ -73,7 +48,7 @@ $(document).ready(function () {
         </header>
         <p class="tweet-text">${tweet.content.text}</p>
         <footer>
-          <div class="timestamp">${tweet.created_at}</div>
+          <div class="timestamp">${timeago.format(tweet.created_at)}</div>
           <div class="actions">
             <i class="fa-solid fa-flag"></i>
             <i class="fa-solid fa-reply"></i>
@@ -85,17 +60,29 @@ $(document).ready(function () {
     return $tweet;
   }
   
-const renderTweets = function(tweets) {
+  const renderTweets = function(tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
-  //  $("#tweets-container").empty();
-  for (const tweet of tweets) {
-    const $tweetElement = createTweetElement(tweet);
-    $("#tweets-container").prepend($tweetElement);
+  // $("#tweets-container").empty();
+    for (const tweet of tweets) {
+      const $tweetElement = createTweetElement(tweet);
+      $("#tweets-container").prepend($tweetElement);
+      }
     }
-  }
-  
-  renderTweets(data);
+
+  // renderTweets(data);
+
+  const loadTweets = function() {
+    $.ajax({
+      url: "/api/tweets",
+      method: "GET",
+      success: function(data) {
+        renderTweets(data);
+      }
+    });
+  };
+
+  loadTweets()
 
 });
